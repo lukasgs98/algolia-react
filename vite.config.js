d2c8f0f6a4563730fa0512020d6a0216cea79c7c
Lukas Geissler <lukasgs_@outlook.de>
2025-05-03 19:20:22 +0200

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/algolia-react',
+  base: '/algolia-react/',
   plugins: [react()],
 })
