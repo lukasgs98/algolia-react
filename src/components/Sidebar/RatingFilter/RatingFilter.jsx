4eb13bc0809cd408edcbc174e36c68a16b277710
Lukas Geissler <lukasgs_@outlook.de>
2025-05-03 19:25:05 +0200

    Fix asset filepath (hopefully)

--- src/components/Sidebar/RatingFilter/RatingFilter.jsx
+++ src/components/Sidebar/RatingFilter/RatingFilter.jsx
@@ -6,12 +6,12 @@ function RatingFilter() {
         <div class="rating-filter">
             <div class="cuisine-filter-title">Rating</div>
             <div class="rating-filter-list">
-                <img id="zero" alt="☆☆☆☆☆" src="algolia-react/public/assets/rating_0_of_5.svg"/>
-                <img id="one" alt="★☆☆☆☆" src="algolia-react/public/assets/rating_1_of_5.svg"/>
-                <img id="two" alt="★★☆☆☆" src="algolia-react/public/assets/rating_2_of_5.svg"/>
-                <img id="three" alt="★★★☆☆" src="algolia-react/public/assets/rating_3_of_5.svg"/>
-                <img id="four" alt="★★★★☆" src="algolia-react/public/assets/rating_4_of_5.svg"/>
-                <img id="five" alt="★★★★★" src="algolia-react/public/assets/rating_5_of_5.svg"/>
+                <img id="zero" alt="☆☆☆☆☆" src="public/assets/rating_0_of_5.svg"/>
+                <img id="one" alt="★☆☆☆☆" src="public/assets/rating_1_of_5.svg"/>
+                <img id="two" alt="★★☆☆☆" src="public/assets/rating_2_of_5.svg"/>
+                <img id="three" alt="★★★☆☆" src="public/assets/rating_3_of_5.svg"/>
+                <img id="four" alt="★★★★☆" src="public/assets/rating_4_of_5.svg"/>
+                <img id="five" alt="★★★★★" src="public/assets/rating_5_of_5.svg"/>
             </div>
         </div>
     );
