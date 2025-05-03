1552dd7bd034d498e79f712cef30508cf590c94f
Lukas Geissler <lukasgs_@outlook.de>
2025-05-03 19:34:05 +0200

    Try again

--- src/components/Sidebar/RatingFilter/RatingFilter.jsx
+++ src/components/Sidebar/RatingFilter/RatingFilter.jsx
@@ -6,12 +6,12 @@ function RatingFilter() {
         <div class="rating-filter">
             <div class="cuisine-filter-title">Rating</div>
             <div class="rating-filter-list">
-                <img id="zero" alt="☆☆☆☆☆" src="/algolia-react/src/components/Sidebar/RatingFilter/rating_0_of_5.svg"/>
-                <img id="one" alt="★☆☆☆☆" src="/algolia-react/src/components/Sidebar/RatingFilter/rating_1_of_5.svg"/>
-                <img id="two" alt="★★☆☆☆" src="/algolia-react/src/components/Sidebar/RatingFilter/rating_2_of_5.svg"/>
-                <img id="three" alt="★★★☆☆" src="/algolia-react/src/components/Sidebar/RatingFilter/rating_3_of_5.svg"/>
-                <img id="four" alt="★★★★☆" src="/algolia-react/src/components/Sidebar/RatingFilter/rating_4_of_5.svg"/>
-                <img id="five" alt="★★★★★" src="/algolia-react/src/components/Sidebar/RatingFilter/rating_5_of_5.svg"/>
+                <img id="zero" alt="☆☆☆☆☆" src="/algolia-react/assets/rating_0_of_5.svg"/>
+                <img id="one" alt="★☆☆☆☆" src="/algolia-react/assets/rating_0_of_5.svg"/>
+                <img id="two" alt="★★☆☆☆" src="/algolia-react/assets/rating_0_of_5.svg"/>
+                <img id="three" alt="★★★☆☆" src="/algolia-react/assets/rating_0_of_5.svg"/>
+                <img id="four" alt="★★★★☆" src="/algolia-react/assets/rating_0_of_5.svg"/>
+                <img id="five" alt="★★★★★" src="/algolia-react/assets/rating_0_of_5.svg"/>
             </div>
         </div>
     );
