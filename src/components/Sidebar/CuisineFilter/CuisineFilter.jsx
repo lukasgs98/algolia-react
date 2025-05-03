7c8529cf980be8642a144c73c992feffc82d0af2
Lukas Geissler <lukasgs_@outlook.de>
2025-05-03 17:09:18 +0200

    Add cuisine filter component

---
+++ src/components/Sidebar/CuisineFilter/CuisineFilter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CuisineFilter() {
+    return (
+        <div class="cuisine-filter">
+            <div class="cuisine-filter-title">Cuisine/Food Type</div>
+            <ul class="cuisine-filter-list">
+                <li>Cuisine A</li>
+                <li>Cuisine B</li>
+            </ul>
+        </div>
+    );
+}
+
+export default CuisineFilter;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import CuisineFilter from "./CuisineFilter/CuisineFilter";
 
 function Sidebar() {
     return (
         <div class="sidebar">
-        
+            <CuisineFilter />
         </div>
     );
 }
