1df5b0b9bd5e3d92dd4cb98e7fe19105df0f0fa9
Lukas Geissler <lukasgs_@outlook.de>
2025-05-04 17:16:21 +0200

    Add dependencies to useEffect hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,25 +8,7 @@ import runSearch from "../algoliasearch.js";
 
 function App() {
 
-    // USE STATE TO REMEMBER SEARCH RESULTS
-    const [results, setResults] = React.useState(null);
 
-    // INITIALIZE SEARCH WITH ALL 5 STAR RESTAURANTS
-    React.useEffect(() => {
-        let query = "";
-        let cuisineFilter = "";
-        let paymentOptionsFilter = "";
-        let ratingsFilter = 5;
-
-        runSearch(query, cuisineFilter, ratingsFilter, paymentOptionsFilter)
-            .then((res) => {
-                setResults(res);
-            }).catch((err) => {
-                console.error("Search error:", err);
-            })
-      }, []);
-
-    if (results) console.log(results);
 
     // USE STATE TO REMEMBER SELECTED CUISINE
     const [selectedCuisines, setSelectedCuisines] = React.useState([]);
@@ -40,7 +22,20 @@ function App() {
             }
         });
     };
-    
+
+    // USE STATE TO REMEMBER SELECTED PAYMENTOPTIONS
+    const [selectedPaymentOptions, setSelectedPaymentOptions] = React.useState([]);
+    function filterPaymentOptions(value) {
+        const paymentOption = value;
+        setSelectedPaymentOptions(prevSelection  => {
+            if (prevSelection.includes(paymentOption)) {
+                return prevSelection.filter(item => item !== paymentOption);
+            } else {
+                return [...prevSelection, paymentOption];
+            }
+        });
+    };    
+
     // USE STATE TO REMEMBER SELECTED RATINGS
     const [selectedRatings, setSelectedRatings] = React.useState([]);
     function filterRatings(value) {
@@ -54,18 +49,25 @@ function App() {
         });
     };
     
-    // USE STATE TO REMEMBER SELECTED PAYMENTOPTIONS
-    const [selectedPaymentOptions, setSelectedPaymentOptions] = React.useState([]);
-    function filterPaymentOptions(value) {
-        const paymentOption = value;
-        setSelectedPaymentOptions(prevSelection  => {
-            if (prevSelection.includes(paymentOption)) {
-                return prevSelection.filter(item => item !== paymentOption);
-            } else {
-                return [...prevSelection, paymentOption];
-            }
-        });
-    };
+    // USE STATE TO REMEMBER SEARCH RESULTS
+    const [results, setResults] = React.useState(null);
+
+    // INITIALIZE SEARCH WITH ALL 5 STAR
+    React.useEffect(() => {
+        let query = "";
+        let cuisineFilter = "";
+        let paymentOptionsFilter = "";
+        let ratingsFilter = 5;
+
+        runSearch(query, cuisineFilter, paymentOptionsFilter, ratingsFilter)
+            .then((res) => {
+                setResults(res);
+            }).catch((err) => {
+                console.error("Search error:", err);
+            })
+      }, [selectedCuisines, selectedPaymentOptions, selectedRatings]);
+
+    if (results) console.log(results);
 
     return (<>
               <div className="top-container">
