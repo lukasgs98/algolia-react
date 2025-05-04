dd020589eea2304b89ff27badd5a23d9b28addb9
Lukas Geissler <lukasgs_@outlook.de>
2025-05-04 09:33:49 +0200

    Add state to CuisineFilter component to keep track of selected cuisines

--- src/components/Sidebar/CuisineFilter/CuisineFilter.jsx
+++ src/components/Sidebar/CuisineFilter/CuisineFilter.jsx
@@ -3,11 +3,25 @@ import ListItem from "./ListItem/ListItem";
 import "./CuisineFilter.css";
 
 function CuisineFilter() {
+
+    const [selectedCuisines, setSelectedCuisines] = React.useState([]);
+    function handleClick(event) {
+        const cuisine = event.target.textContent;
+        setSelectedCuisines(prevSelection => {
+            if (prevSelection.includes(cuisine)) {
+                return prevSelection.filter(item => item !== cuisine);
+            } else {
+                return [...prevSelection, cuisine];
+            }
+        });
+    };
+
     return (
         <div className="cuisine-filter">
             <div className="cuisine-filter-title">Cuisine/Food Type</div>
             <ul className="cuisine-filter-list">
-                <ListItem id="1" textContent="Asian" />
+                <ListItem id="1" onClick={handleClick} textContent="Asian" />
+                <ListItem id="2" onClick={handleClick} textContent="Mexican" />
             </ul>
         </div>
     );
